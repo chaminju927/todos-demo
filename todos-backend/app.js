bc9427b96c528c807ae130ba9e6b47eb5f1de3a5
chaminju <minjoo92718@gmail.com>
2024-10-18 23:58:29 +0900

    package파일 스크립트 추가

--- todos-backend/app.js
+++ todos-backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const app = express();
+require("dotenv").config();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("uri", MONGODB_URI_PROD);
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", indexRouter); // api라는 주소로 호출 오면 indexrouter로 가고-> 거기서 /task있으면 task.api로 이동
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(
